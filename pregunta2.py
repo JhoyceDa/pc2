367fb4d6999cc32f93a844a10073062ff0af8cfa
alumno informatica <alumno@ciis.usil.edu.pe>
2019-06-25 08:35:54 -0500

    pregunta2

--- pregunta2.py
+++ pregunta2.py
@@ -1,3 +1,12 @@
 ## Una historia de usuario es un escrito que permite al programador poder
 ##identificar las necesidades o problemas que tiene al momento de utilizar el software
-## Un ejemplo de esto es 
+## Un ejemplo:
+## Se quiere saber el costo en cantidad del producto que compro en una tienda online
+
+def summa():
+    suma=0
+    costo= 3
+    m =int(input("Que cantidad de este producto va comprar"))
+    suma = m*3
+    
+    print("El costo total es: " + suma )
