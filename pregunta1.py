103f178375340725b4617947e1b61d79c264e650
alumno informatica <alumno@ciis.usil.edu.pe>
2019-06-25 08:27:03 -0500

    pregunta2

---
+++ pregunta1.py
@@ -0,0 +1,15 @@
+
+m = [[5,4,7],[6,7,8],[1,2,3]]
+n = [[1,2,3,4,],[4,5,6,7],[7,8,9,4]]
+p = [[1.2],[5,4]]
+suma =0
+def matriz():
+     o= int(input("Ingrese las fila de una matriz cuadrada"))
+     m = input("Ingrese el nombre de la matriz")
+     for i in range(o):    
+         suma = suma + m[i][i]
+    if suma %2==0:
+        print("la suma es par")
+    else:
+        print("la suma no es par")
+     print(suma)
---
+++ pregunta2.py
@@ -0,0 +1,3 @@
+## Una historia de usuario es un escrito que permite al programador poder
+##identificar las necesidades o problemas que tiene al momento de utilizar el software
+## Un ejemplo de esto es 
